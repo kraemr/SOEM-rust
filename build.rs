09997313c0202c3d51d0d66e4f2f5521f0cbb0f9
rob <93094049+kraemr@users.noreply.github.com>
2025-11-20 17:04:26 +0100

    added bindings

---
+++ build.rs
@@ -0,0 +1,34 @@
+
+extern crate bindgen;
+extern crate cc;
+
+fn main() {
+    cc::Build::new()
+        .files([
+            "soem/src/ec_base.c","soem/src/ec_coe.c","soem/src/ec_config.c",  "soem/src/ec_dc.c",  "soem/src/ec_eoe.c",  "soem/src/ec_foe.c",  "soem/src/ec_main.c",  "soem/src/ec_print.c",  "soem/src/ec_soe.c"
+        ])
+        .include("soem/build/include/")
+	.include("soem/include")
+	.include("soem/osal/linux")
+	.include("soem/osal")
+	.include("soem/oshw/linux/")
+        .compile("soem");
+
+
+    // Generate Rust bindings with include path fixes
+    let bindings = bindgen::Builder::default()
+        .header("soem/include/soem/soem.h")
+        .clang_arg("-Isoem/include")
+        .clang_arg("-Isoem/build/include")   // <- important for ec_options.h
+        .clang_arg("-Isoem/osal")
+        .clang_arg("-Isoem/osal/linux")
+        .clang_arg("-Isoem/oshw/linux")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file("src/bindings.rs")
+        .expect("Couldn't write bindings!");
+
+
+}
